b7aabfb4db4157146b2e329278d9e2390865d57d
Lordwill <messagelordwill@gmail.com>
2023-06-26 14:35:37 -0700

    Updated

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# automated puppet fix (to find out why Apache is returning a 500 error)
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+### 0x17. Web stack debugging #3
